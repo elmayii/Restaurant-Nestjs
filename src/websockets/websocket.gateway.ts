caf9f65552276baab49979290daca306212d9d89
elmayii <mariomanuel.pereravaldes@gmail.com>
2024-07-15 09:48:50 -0400

    feat: :zap: gateway restore

--- src/websockets/websocket.gateway.ts
+++ src/websockets/websocket.gateway.ts
@@ -1,4 +1,3 @@
-import { UnauthorizedException, UseGuards } from '@nestjs/common';
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
@@ -7,15 +6,11 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { AccessGuard } from 'src/auth/auth.guard';
-// import { SocketGuard } from 'src/websockets/auth.socketGuard';
 
-//@UseGuards(AccessGuard)
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
-  
 })
 export class WebsocketGateway
   implements OnGatewayConnection, OnGatewayDisconnect
@@ -24,17 +19,9 @@ export class WebsocketGateway
   server: Server;
   private activeClients: Map<string, string> = new Map();
 
-
   handleConnection(client: Socket) {
-    try {
-      console.log("Client conected:",client.id)
-      //this.activeClients.set(client.id, client.handshake.auth.name);
-      //console.log(client.handshake.auth);
-    } catch (error) {
-      throw new UnauthorizedException({message:"Unauthorized"})
-    }
+    console.log(`Client connected: ${client.id}`);
   }
-
 
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
@@ -44,20 +31,14 @@ export class WebsocketGateway
   @SubscribeMessage('register')
   handleRegister(client: Socket, userId: string) {
     this.activeClients.set(client.id, userId);
-    this.notifyUser(client.id, {
-      message: 'Auth',
-    });
   }
 
   notifyUser(userId: string, message: any) {
-    //console.log(userId)
     for (const [socketId, id] of this.activeClients.entries()) {
-      //console.log('otro id:', id)
-      // if (id === userId) {
-        
-      // }
-      this.server.to(socketId).emit('notification', message);
+      if (id === userId) {
+        this.server.to(socketId).emit('notification', message);
         break;
+      }
     }
   }
 }
